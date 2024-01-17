e67aaec53ce55e4425acc013334150ba97fc9b54
Alexey Mikhaylov <krummi1414@yandex.ru>
2024-01-17 15:16:22 +0300

    feat: add all components

---
+++ src/app/components/application/application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application',
+  standalone: true,
+  imports: [],
+  templateUrl: './application.component.html',
+  styleUrl: './application.component.scss'
+})
+export class ApplicationComponent {
+
+}
---
+++ src/app/components/header/header-offer/header-offer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-offer',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-offer.component.html',
+  styleUrl: './header-offer.component.scss'
+})
+export class HeaderOfferComponent {
+
+}

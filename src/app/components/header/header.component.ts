7c71efdb651bfaef50fab8f3d64914abf54042b5
Alexey Mikhaylov <krummi1414@yandex.ru>
2024-01-18 20:13:40 +0300

    feat: add header block

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderOfferComponent } from './header-offer/header-offer.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [HeaderOfferComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })

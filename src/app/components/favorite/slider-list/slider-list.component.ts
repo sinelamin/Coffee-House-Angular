35ec3b6b00ab990d546bfbd016eb5d7a8a94406f
Alexey Mikhaylov <krummi1414@yandex.ru>
2024-01-26 00:08:39 +0300

    feat: add functional for slider

--- src/app/components/favorite/slider-list/slider-list.component.ts
+++ src/app/components/favorite/slider-list/slider-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
 import { SliderCardComponent } from '../slider-card/slider-card.component';
 import dataSlider from '../../../../assets/data/dataSlider.json';
 
@@ -11,6 +11,20 @@ import dataSlider from '../../../../assets/data/dataSlider.json';
   templateUrl: './slider-list.component.html',
   styleUrl: './slider-list.component.scss'
 })
-export class SliderListComponent {
+
+export class SliderListComponent implements OnChanges {
   dataSliderCards = dataSlider.coffee;
+
+  @Input() anyValue: any;
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const inputName in changes) {
+      const inputValues = changes[inputName];
+      // console.log(`Previous ${inputName} == ${inputValues.previousValue}`);
+      // console.log(`Current ${inputName} == ${inputValues.currentValue}`);
+      // console.log(`Is first ${inputName} change == ${inputValues.firstChange}`);
+    }
+
+    // console.log('anyValue changed:', this.anyValue);
+  }
 }
--- src/app/components/favorite/slider/slider.component.ts
+++ src/app/components/favorite/slider/slider.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { SliderListComponent } from '../slider-list/slider-list.component';
+import dataSlider from '../../../../assets/data/dataSlider.json';
 
 @Component({
   selector: 'app-slider',
@@ -11,5 +12,82 @@ import { SliderListComponent } from '../slider-list/slider-list.component';
   styleUrl: './slider.component.scss'
 })
 
-export class SliderComponent {
+export class SliderComponent implements OnInit, AfterViewInit {
+
+  startPoint = 0;
+  numberOfCards = dataSlider.coffee.length;
+  position = this.startPoint;
+  offset = 480;
+  maxRightPosition = this.offset * (this.numberOfCards - 1);
+
+  progress = 0;
+  index = 0;
+  arrProgressBars: ElementRef[] = [];
+
+  moveRight() {
+    this.progress = 0;
+
+    if (this.position === -this.maxRightPosition) {
+      this.position = 0;
+      this.getProgressBarWidth();
+      this.index = 0;
+    } else {
+      this.position -= this.offset;
+      this.getProgressBarWidth();
+      this.index += 1;
+    }
+
+    return this.position;
+  }
+
+  moveLeft() {
+    this.progress = 0;
+
+    if (this.position == 0) {
+      this.position = -this.maxRightPosition;
+      this.getProgressBarWidth();
+      this.index = this.numberOfCards - 1;
+    } else {
+      this.position += this.offset;
+      this.getProgressBarWidth();
+      this.index -= 1;
+    }
+
+    return this.position;
+  }
+
+
+  @ViewChildren('progress') progressBars!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+    this.arrProgressBars = this.progressBars.toArray();
+    this.growthOfProgress();
+  }
+
+  getProgressBarWidth() {
+    this.arrProgressBars[this.index].nativeElement.style.width = `${this.progress}%`;
+  }
+
+  growthOfProgress(): void {
+    setTimeout(() => {
+      this.progress += 1;
+      this.growthOfProgress();
+    }, 20);
+
+    this.getProgressBarWidth();
+
+    if (this.progress === 100) {
+      this.progress = 0;
+      this.getProgressBarWidth();
+      this.moveRight();
+    }
+
+    if (this.index > this.numberOfCards - 1) {
+      this.index = 0;
+    }
+  }
+
+  ngOnInit(): void {
+    // this.growthOfProgress();
+  }
 }

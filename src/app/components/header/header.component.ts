62353397189659d667ef4d88ef4c420181f7fe72
Alexey Mikhaylov <krummi1414@yandex.ru>
2024-02-25 20:04:03 +0300

    feat: add adaptive for 768px

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
   scrollToFooter(): void {
     document.querySelector('#footer')?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  burgerActiveState: boolean = false;
+
+  burgerActive() {
+    this.burgerActiveState = !this.burgerActiveState;
+  }
+
+  navigateClick() {
+    this.burgerActiveState = false;
   }
 }
